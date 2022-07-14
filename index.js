a18f620d21a0e18f1ecfe8b71ce5768638a362ab
raihan <mostafa.raihan@newscred.com>
2022-07-14 12:58:05 +0600

    use redis to cache response from api

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 const path = require("path");
 const express = require("express");
-
+const redis = require("redis");
 const { fetch } = require("./fetch");
 
 const app = express();
 
 const PORT = 8000;
+const REDIS_PORT = 6379;
+
+let redisClient;
+
+(async () => {
+  redisClient = redis.createClient(REDIS_PORT);
+
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+  await redisClient.connect();
+  console.log("connected");
+})();
 
 app.listen(PORT, () => console.log("Listening..."));
 
-app.get("/", async (req, res) => {
+async function cache(req, res, next) {
+  console.log("calling cache");
+  let data = await redisClient.get("data");
+
+  if (data != null) {
+    console.log("from cache");
+    data = JSON.parse(data);
+    res.json({ length: data.length, data });
+  } else {
+    console.log("not found in cache");
+    next();
+  }
+}
+
+app.get("/", cache, async (req, res) => {
   // res.sendFile(path.join(__dirname + '/index.html'));
   const data = await fetch();
-  return res.json({ data, length: data.length });
+  console.log("not from cache");
+  await redisClient.setEx("data", 120, JSON.stringify(data));
+  return res.json({ length: data.length, data });
 });
 
 app.get("/search", async (req, res) => {
   const q = req.query.q;
-  if (q) {
-    const data = await fetch(q);
-    return res.json(data);
+  try {
+    if (q) {
+      const data = await fetch(q);
+      return res.json(data);
+    }
+    return res.send("You have not searched for anything");
+  } catch (err) {
+    return res.status(500).send("Hah");
   }
-
-  res.send("You have not searched for anything");
 });
